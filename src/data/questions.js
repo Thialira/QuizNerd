4287776a22f37965497778efd74b6f6d645d491f
Thiago Jefferson Cordeiro Mendes de lira <tjlira07@gmail.com>
2022-11-09 19:59:34 -0300

    Quiz nerd

---
+++ src/data/questions.js
@@ -0,0 +1,45 @@
+const data = [
+  {
+    question: 'Quem planeou o assalto em La Casa de Papel?',
+    options: ['Nairobi', 'Berlin', 'Rio', 'Professor'],
+    answer: 'Professor',
+  },
+  {
+    question: 'Quem matou Oberyn Martell?',
+    options: ['Perdigueiro', 'Gregor Clegane', 'Jaime Lannister', 'Arya Stark'],
+    answer: 'Gregor Clegane',
+    tip: 'Também, é chamado de A Montanha',
+  },
+  {
+    question: 'Que livro a Wanda estuda no final de WandaVision?',
+    options: [
+      'Livro dos Vichantes',
+      'HoodDark',
+      'Darkhood',
+      'Livro de Cagliostro',
+    ],
+    answer: 'Darkhood',
+  },
+  {
+    question: 'Quem é Wanda Maximorf?',
+    options: [
+      'Feiticeira Escarlate',
+      'Uma Bruxa',
+      'Uma Multante',
+      'Uma Skrull',
+    ],
+    answer: 'Feiticeira Escarlate',
+  },
+  {
+    question: 'Por que loki é preso pela AVT?',
+    options: [
+      'Porque fingiu ser Odin',
+      'Porque matou Thor',
+      'Porque fugiu com o tesseract',
+      'Porque forjou  a própria morte',
+    ],
+    answer: 'Porque fugiu com o tesseract',
+  },
+];
+
+export default data;
---
+++ src/data/questions_complete.js
@@ -0,0 +1,132 @@
+const data = [
+  {
+    category: 'Filmes',
+    questions: [
+      {
+        question: 'Qual o nome do pai de Luke Skywalker?',
+        options: ['Palpatine', 'Han Solo', 'Darth Vader', 'Obi-Wan Kenobi'],
+        answer: 'Darth Vader',
+      },
+      {
+        question:
+          'Qual horcrux estava guardado no cofre de Bellatrix Lestranger?',
+        options: [
+          'O Medalhão de Salazar Sonserina',
+          'Nagini',
+          'A Tiara de Rowena Corvinal',
+          'Taça de ouro de Helga Hufflepuff',
+        ],
+        answer: 'Taça de ouro de Helga Hufflepuff',
+        tip: 'Quando se Vence um Campeonato ganha-se uma...',
+      },
+      {
+        question: 'Qual destes filmes da Disney foi Lançado primeiro?',
+        options: ['Pocahontas', 'O Rei Leão', 'Mulan', 'Toy Story'],
+        answer: 'O Rei Leão',
+      },
+      {
+        question: 'Em que filme surge a frase: “Para o infinito e mais além”?',
+        options: ['Toy Story', 'Tarzan', 'Peter Pan', 'Lilo & Stitch'],
+        answer: 'Toy Story',
+      },
+      {
+        question:
+          'Em qual classe de Mago Gandalf começa em "A sociedade do anel"?',
+        options: ['Mago branco', 'Mago cinzento', 'Mago azul', 'Mago preto'],
+        answer: 'Mago cinzento',
+      },
+    ],
+  },
+  {
+    category: 'Animes',
+    questions: [
+      {
+        question: 'Os poderes de Luffy de One Piece, são baseados em que?',
+        options: ['Fogo', 'Borracha', 'Ferro', 'Mola'],
+        answer: 'Borracha',
+      },
+      {
+        question:
+          'Ao pegar todas as Esferas do Dragão é concedido a pessoa que as encontrou?',
+        options: ['Virar um Sayajin', 'Um desejo', 'Teletransporte', 'Poder'],
+        answer: 'Um desejo',
+      },
+      {
+        question:
+          'Quem foi o primeiro demon slayer com quem Tanjiro interagiu na história?',
+        options: [
+          'Muzan Kibutsuji',
+          'Giyu Tomioka',
+          'Zenitsu Agatsuma',
+          'Sakonji Urokodaki',
+        ],
+        answer: 'Giyu Tomioka',
+        tip: 'Ele é conhecido por ser o Hashira da Água',
+      },
+      {
+        question:
+          'Durante o arco do trem infinito, como Tanjiro conseguiu despertar do sonho criado para ele por Enmu?',
+        options: [
+          'Se matando no sonho',
+          'Com um chute de Nezuko',
+          'Com a técnica da respiração da água',
+          'Com ajuda de Rengoku',
+        ],
+        answer: 'Se matando no sonho',
+      },
+    ],
+  },
+  {
+    category: 'Series',
+    questions: [
+      {
+        question: 'Quem planejou o assalto em La Casa de Papel?',
+        options: ['Nairobi', 'Berlin', 'Rio', 'Professor'],
+        answer: 'Professor',
+      },
+      {
+        question: 'Quem matou Oberyn Martell?',
+        options: [
+          'Perdigueiro',
+          'Gregor Clegane',
+          'Jaime Lannister',
+          'Arya Stark',
+        ],
+        answer: 'Gregor Clegane',
+        tip: 'Também, é chamado de A Montanha',
+      },
+      {
+        question: 'Que livro a Wanda estuda no final de WandaVision?',
+        options: [
+          'Livro dos Vichantes',
+          'HoodDark',
+          'Darkhood',
+          'Livro de Cagliostro',
+        ],
+        answer: 'Darkhood',
+      },
+      {
+        question: 'Quem é Wanda Maximorf?',
+        options: [
+          'Feiticeira Escarlate',
+          'Uma Bruxa',
+          'Uma Multante',
+          'Uma Skrull',
+        ],
+        answer: 'Feiticeira Escarlate',
+      },
+      {
+        question: 'Por que loki é preso pela AVT?',
+        options: [
+          'Porque fingiu ser Odin',
+          'Porque matou Thor',
+          'Porque fugiu com o tesseract',
+          'Porque forjou  a própria morte',
+        ],
+        answer: 'Porque fugiu com o tesseract',
+      },
+    ],
+  },
+];
+
+export default data;
